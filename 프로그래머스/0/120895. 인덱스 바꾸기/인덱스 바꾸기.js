09d6eb1257993b04066f7c61339b06b918aa21d3
mminsong <105476298+mminsong@users.noreply.github.com>
2024-04-15 17:24:16 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,11 @@
+function solution(my_string, num1, num2) {
+    var arr=[...my_string];
+    
+    var str1=arr[num1];
+    var str2=arr[num2];
+    
+    arr.splice(num1,1,str2);
+    arr.splice(num2,1,str1);
+    
+    return arr.join("");
+}
